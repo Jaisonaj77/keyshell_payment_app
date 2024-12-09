34b5f0afff72a32165f7fe9035b34f10213f8a8d
Jaisonaj77 <jaisonaj77@gmail.com>
2024-12-09 11:58:23 +0530

    Update environment.ts

--- micro-services/frontend/src/environments/environment.ts
+++ micro-services/frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://13.232.83.254/api',
-    authServiceUrl: 'http://13.232.83.254/api/auth',
-    paymentServiceUrl: 'http://13.232.83.254/api/payments'
+    apiUrl: 'http://13.202.69.102/api',
+    authServiceUrl: 'http://13.202.69.102/api/auth',
+    paymentServiceUrl: 'http://13.202.69.102/api/payments'
   };
